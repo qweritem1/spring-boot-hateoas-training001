25b422c7f62e8a66d7ab572e4cc9ff29d5d45814
1003642 <a1003642@SKP1003642MN002.local>
2018-11-04 15:43:36 +0900

    Make Model

--- src/main/java/hateoas/model/Customer.java
+++ src/main/java/hateoas/model/Customer.java
@@ -1,4 +1,62 @@
 package hateoas.model;
 
-public class Customer {
+import org.springframework.hateoas.ResourceSupport;
+
+public class Customer extends ResourceSupport {
+    private String customerId;
+    private String customerName;
+    private String companyName;
+    private String orders;
+
+    public Customer() {
+        super();
+    }
+
+    public Customer(String customerId, String customerName, String companyName) {
+        super();
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.companyName = companyName;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getOrders() {
+        return orders;
+    }
+
+    public void setOrders(String orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId='" + customerId + '\'' +
+                ", customerName='" + customerName + '\'' +
+                ", companyName='" + companyName + '\'' +
+                '}';
+    }
 }
--- src/main/java/hateoas/model/Order.java
+++ src/main/java/hateoas/model/Order.java
@@ -1,4 +1,53 @@
 package hateoas.model;
 
-public class Order {
+import org.springframework.hateoas.ResourceSupport;
+
+public class Order extends ResourceSupport {
+    private String orderId;
+    private double price;
+    private int quantity;
+
+    public Order() {
+        super();
+    }
+
+    public Order(String orderId, double price, int quantity) {
+        super();
+        this.orderId = orderId;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
